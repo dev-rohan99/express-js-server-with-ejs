fbb4bf7a9cb585e2c988426792e69197c451fd96
rohan-mostofa <rohanmostofa599@gmailcom>
2022-07-04 21:21:56 +0600

    All files upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const dotenv = require('dotenv').config();
 
 // environment variable
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} PORT...`);
